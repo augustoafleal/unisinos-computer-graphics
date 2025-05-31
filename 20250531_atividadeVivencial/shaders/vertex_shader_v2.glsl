b004cd0a47e7fa11356890074652b1ddd8901a8b
Augusto Leal <augustoafleal@gmail.com>
2025-05-31 19:25:37 -0300

    atividade_vivencial

---
+++ 20250531_atividadeVivencial/shaders/vertex_shader_v2.glsl
@@ -0,0 +1,21 @@
+#version 460 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 uv;
+
+out vec3 fragPos;
+out vec3 fragNormal;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    vec4 worldPos = model * vec4(position, 1.0);
+    gl_Position = projection * view * worldPos;
+
+    fragPos = vec3(worldPos);
+    fragNormal = vec3(model * vec4(normal, 1.0));
+}
